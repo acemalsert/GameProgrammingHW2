690dc4b222cba8ca4c69de40708da87dbedc29b6
Ahmet Cemal Sert <acemalsert@gmail.com>
2022-05-07 16:12:44 +0300

    final commit

--- Assets/Scripts/GameObjectMovement.cs
+++ Assets/Scripts/GameObjectMovement.cs
@@ -1,47 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameObjectMovement : MonoBehaviour
 {
 
-    public float speed = 5f;
-    public float rotaionTreshold = 100f;
-    private float x_axis;
-    private float y_axis;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] 
+    private Transform movePositionTransform;
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+
+    private void Awake(){
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        fixedSpeedMove();
-        horizontalRotataion();
-        verticalRotation();
+    private void Update(){
+        navMeshAgent.destination = movePositionTransform.position;
     }
-
-    void fixedSpeedMove()
-    {
-        transform.Translate(0, 0, speed * Time.deltaTime);
-    }
-
-     void horizontalRotataion()
-    {
-        x_axis = Input.GetAxis("Horizontal");
-        if (!(x_axis == 0))
-        {
-            transform.Rotate(0f, 0f, x_axis * rotaionTreshold * Time.deltaTime);
-        }
-    }
-    void verticalRotation()
-    {
-        y_axis = Input.GetAxis("Vertical");
-        if(!(y_axis == 0))
-        {
-            transform.Rotate(y_axis * rotaionTreshold * Time.deltaTime,0f,0f);
-        }
-    }
+    
 }
